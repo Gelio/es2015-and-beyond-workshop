bbbc0767db0e3cac23a85bd5f5796ebf5a02925c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-06 14:01:28 +0100

    solve 05-bound-methods

--- exercises/05-bound-methods/05-bound-methods.js
+++ exercises/05-bound-methods/05-bound-methods.js
@@ -1,14 +1,9 @@
-/**
- * TODO: bind the `getSum` method so that `this` inside of it always points to the created object.
- *
- * Take a look at
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
- */
-
 class BoundMethods {
   constructor(a, b) {
     this.a = a;
     this.b = b;
+
+    this.getSum = this.getSum.bind(this);
   }
 
   getSum() {
