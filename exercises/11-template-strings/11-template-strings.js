858d7bf41018743f4c084e839ff2556a4f4ac78f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-06 14:02:10 +0100

    solve 11-template-strings

--- exercises/11-template-strings/11-template-strings.js
+++ exercises/11-template-strings/11-template-strings.js
@@ -1,27 +1,14 @@
-/**
- * NOTE:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
- */
-
 const age = 42;
 
 const regularAge = 'The age is ' + age;
 
-/**
- * TODO: complete the `templateAge` variable to be the same as `regularAge` but using a template
- * string
- */
-const templateAge = null;
+const templateAge = `The age is ${age}`;
 
 const multilineString = 'Line one\nLine two\nLine three';
 
-/**
- * TODO: complete the `multilineTemplateString` variable to be the same as `multilineString` but
- * using a template string.
- *
- * NOTE: Do not use the `\n` character explicitly
- */
-const multilineTemplateString = null;
+const multilineTemplateString = `Line one
+Line two
+Line three`;
 
 module.exports = {
   regularAge,
