c202a14238b8c5bb4f0cea4889ee448a0cd62603
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-06 14:01:55 +0100

    solve 08-destructuring

--- exercises/08-destructuring/08-destructuring.js
+++ exercises/08-destructuring/08-destructuring.js
@@ -1,11 +1,3 @@
-/**
- * TODO:
- * Convert the syntax in each function to use destructuring
- *
- * NOTE:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
- */
-
 const regularObjectDestructuring = () => {
   const obj = {
     foo: 1,
@@ -13,12 +5,7 @@ const regularObjectDestructuring = () => {
     baz: [42, 42, 42]
   };
 
-  /**
-   * TODO: extract `foo` and `baz` from `obj` and store them as variables.
-   */
-
-  const foo = obj.foo;
-  const baz = obj.baz;
+  const { foo, baz } = obj;
 
   return { foo: foo, baz: baz };
 };
@@ -30,10 +17,7 @@ const objectDestructuringWithRename = () => {
     bar: 3
   };
 
-  /**
-   * TODO: extract `foo` from `obj` and save it as `renamedFoo`
-   */
-  const renamedFoo = obj.foo;
+  const { foo: renamedFoo } = obj;
 
   return renamedFoo;
 };
@@ -47,12 +31,10 @@ const nestedObjectDestructuring = () => {
     baz: 3
   };
 
-  /**
-   * TODO: extract `foo`, `bar` and `baz` from `obj`
-   */
-  const foo = obj.nested.foo;
-  const bar = obj.nested.bar;
-  const baz = obj.baz;
+  const {
+    nested: { foo, bar },
+    baz
+  } = obj;
 
   return { foo, bar, baz };
 };
@@ -60,11 +42,7 @@ const nestedObjectDestructuring = () => {
 const arrayDestructuring = () => {
   const array = [1, 2, 3, 4, 5];
 
-  /**
-   * TODO: extract the first two elements from `array` and save them as `first` and `second`
-   */
-  const first = array[0];
-  const second = array[1];
+  const [first, second] = array;
 
   return [first, second];
 };
@@ -72,22 +50,12 @@ const arrayDestructuring = () => {
 const secondElementFromTuple = () => {
   const tuple = [1, 2];
 
-  /**
-   * TODO: extract the second element from `tuple` and save it as `second`. Do not extract the first
-   * element.
-   */
-  const second = tuple[1];
+  const [, second] = tuple;
 
   return second;
 };
 
-const parameterDestructuring = (objectParameter, arrayParameter) => {
-  /**
-   * TODO: Extract `foo` and `first` directly from the list of parameters
-   */
-  const foo = objectParameter.foo;
-  const first = arrayParameter[0];
-
+const parameterDestructuring = ({ foo }, [first]) => {
   return { foo, first };
 };
 
