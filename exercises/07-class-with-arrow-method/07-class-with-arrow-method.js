503ec48ca4b4d55b0dd45fe4118998c4dabd43c5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-06 14:01:48 +0100

    solve 07-class-with-arrow-method

--- exercises/07-class-with-arrow-method/07-class-with-arrow-method.js
+++ exercises/07-class-with-arrow-method/07-class-with-arrow-method.js
@@ -1,19 +1,8 @@
-/**
- * TODO: converts `returnSomeArg` to an arrow function so that it does not have to be bound.
- *
- * NOTE: due to not having _babel_ or some other transpiler, you cannot use the function property
- * syntax
- */
-
 class ClassWithArrowMethod {
   constructor(someArg) {
     this.someArg = someArg;
 
-    this.returnSomeArg = this.returnSomeArg.bind(this);
-  }
-
-  returnSomeArg() {
-    return this.someArg;
+    this.returnSomeArg = () => this.someArg;
   }
 }
 
